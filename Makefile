f9db9f97a50e2c42968d244162cf6fd381d18af2
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2024-04-18 20:25:34 +0200

    update README, update Makefile, update test_flask

--- Makefile
+++ Makefile
@@ -4,10 +4,22 @@
 install:
 	pip install -r requirements.txt
 
+.PHONY: flask_app
+flask_app:
+	python flask_app.py
+
+.PHONY: tkinter_app
+tkinter_app:
+	python tkinter_app.py
+
 .PHONY: format
 format:
 	black .
 	ruff check . --fix --exit-zero
+
+.PHONY: test
+test:
+	pytest tests
 
 .PHONY: unit_test
 unit_test:
--- tests/unit_tests/test_flask.py
+++ tests/unit_tests/test_flask.py
@@ -4,6 +4,21 @@ from flask_app import app
 
 
 class TestFlaskApp(unittest.TestCase):
+
+    SPAM_MAIL = (
+        "Dear Homeowner,\n\n \n\nInterest Rates are at their lowest point in 40 years!\n\n\n\nWe help you "
+        "find the best rate for your situation by\n\nmatching your needs with hundreds of lenders!\n\n\n\n"
+        "Home Improvement, Refinance, Second Mortgage,\n\nHome Equity Loans, and More! Even with less than"
+        "\n\nperfect credit!\n\n\n\nThis service is 100% FREE to home owners and new\n\nhome buyers without "
+        "any obligation. \n\n\n\nJust fill out a quick, simple form and jump-start\n\nyour future plans "
+        "today!\n\n\n\n\n\nVisit http://61.145.116.186/user0201/index.asp?Afft=QM10\n\n\n\n\n\n\n\n\n\n\n\n"
+        "\n\nTo unsubscribe, please visit:\n\n\n\nhttp://61.145.116.186/light/watch.asp\n"
+    )
+    NO_SPAM_MAIL = (
+        "Hi Alex\n\nI would like to write to you because I like you and I can`t wait to see you!\n\n"
+        "Regards\nMateusz Szewczyk\n"
+    )
+
     def setUp(self):
         self.app = app.test_client()
 
@@ -20,7 +35,7 @@ class TestFlaskApp(unittest.TestCase):
         mock_vectorizer.transform.return_value = "vectorized_email"
         mock_classifier.predict.return_value = [1]
 
-        response = self.app.post("/classify", data={"email_text": "spam email"})
+        response = self.app.post("/classify", data={"email_text": self.SPAM_MAIL})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json["result"], "Email is spam!")
 
